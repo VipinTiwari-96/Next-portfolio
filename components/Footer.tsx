3fd5b7d54abff53c0b77a853e2409733fed39e36
vipin tiwari <vipin.tiwari@devslane.com>
2024-12-10 16:37:44 +0530

    copy text functionality

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,34 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 // helper
 import { socialLinks, theme } from "../helper/theme";
 // shared
 import Link from "./shared/Link";
 
 const Footer: FC = () => {
+  const [isEmailCopied, setIsEmailCopied] = useState<boolean>(false);
+  const [isNumberCopied, setIsNumberCopied] = useState<boolean>(false);
+
+  const handleEmailCopy = () => {
+    setIsEmailCopied(true);
+    navigator.clipboard.writeText(" vipinvipin1997vipin@gmail.com");
+  };
+
+  const handleNumberCopy = () => {
+    setIsNumberCopied(true);
+    navigator.clipboard.writeText("9634963546");
+  };
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setIsEmailCopied(false);
+      setIsNumberCopied(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [isEmailCopied, isNumberCopied]);
+
   return (
     <div className="bg-gray-100 border-t-2 rounded-t-[90px] p-20 mt-40">
       <div className="w-2/3 mx-auto  text-center h-[15rem] flex flex-col gap-20">
@@ -13,15 +37,31 @@ const Footer: FC = () => {
         </div>
 
         <div
-          className={`lg:w-1/2 mx-auto flex flex-col items-center gap-5 text-[${theme.text.size.phone}]`}
+          className={`lg:w-1/2 mx-auto flex flex-col items-center gap-5 text-[12px] xl:text-[16px]`}
         >
           <div className="flex  w-full justify-between flex-wrap items-center gap-2">
             <theme.icons.email size={30} />
-            <span>vipinvipin1997vipin@gmail.com</span>
+            <span className="flex gap-5 items-center">
+              vipinvipin1997vipin@gmail.com{" "}
+              <span
+                className="bg-white p-1 rounded-md cursor-pointer w-16"
+                onClick={handleEmailCopy}
+              >
+                {isEmailCopied ? "copied!" : "copy"}
+              </span>
+            </span>
           </div>
           <div className="flex  w-full justify-between flex-wrap items-center gap-2">
             <theme.icons.whatsApp size={30} />
-            <span>9634963546</span>
+            <span className="flex gap-5 items-center">
+              9634963546{" "}
+              <span
+                className="bg-white p-1 rounded-md cursor-pointer w-16"
+                onClick={handleNumberCopy}
+              >
+                {isNumberCopied ? "copied!" : "copy"}
+              </span>
+            </span>
           </div>
 
           <div className="flex  w-full justify-between flex-wrap items-center gap-2">
